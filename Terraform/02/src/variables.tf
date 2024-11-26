379f1876408736007224abb85c80c87ac50a1113
AlekseyStroitelev <makaron.str@gmail.com>
2024-11-26 04:55:02 +0000

    Terraform02

---
+++ Terraform/02/src/variables.tf
@@ -0,0 +1,40 @@
+###cloud vars
+
+
+variable "cloud_id" {
+  type        = string
+  default     = "b1gj2n9n1isp9elpqjgg"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1g6kcqbrg8j96ko3b54"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ+eCdyUkojw4hF52nM/4WO8P53gbu+Q5A1n56r0b5VL"
+  description = "ssh-keygen -t ed25519"
+}
